49ad806342a43752d0703714252c963adaaa72e5
Jaël Champagne Gareau <champagne_gareau.jael@courrier.uqam.ca>
2024-02-22 16:23:02 -0500

    add support for api_key login

--- RedOpsAPI.py
+++ RedOpsAPI.py
@@ -61,24 +61,28 @@ class RequestException(Exception):
     pass
 
 class RED_OPS_API:
-    def __init__(self, username=None, password=None, session_cookie=None, endpoint=None, totp=None):
-        self.session = mechanicalsoup.StatefulBrowser()
-        self.session.session.headers.update(headers)
-        self.browser = None
+    def __init__(self, username=None, password=None, session_cookie=None, endpoint=None, totp=None, api_key=None):
+        self.session = requests.Session()
+        self.session.headers.update(headers)
         self.username = username
         self.password = password
         self.session_cookie = session_cookie
         self.totp = totp
         self.endpoint = endpoint
+        self.api_key = api_key
+        self.api_key_authenticated = False
         self.authkey = None
         self.passkey = None
         self.userid = None
         self.last_request = time.time()
+        self.page_size = 2000
         self.rate_limit = 2.0 # seconds between requests
         self._login()
 
     def _login(self):
-        if self.session_cookie is not None:
+        if self.api_key is not None and len(self.api_key) > 0:
+            self._login_api_key()
+        elif self.session_cookie is not None and len(str(self.session_cookie)) > 0:
             try:
                 self._login_cookie()
             except:
@@ -87,23 +91,35 @@ class RED_OPS_API:
         else:
             self._login_username_password()
 
+    def _get_account_info(self):
+        accountinfo = self.request('index')
+        if accountinfo is None:
+            raise LoginException
+        self.authkey = accountinfo['authkey']
+        self.passkey = accountinfo['passkey']
+        self.userid = accountinfo['id']
+
+    def _login_api_key(self):
+        self.session.headers.update({'Authorization': self.api_key})
+        self._get_account_info()
+        self.api_key_authenticated = True
+
     def _login_cookie(self):
         mainpage = '{0}/login.php'.format(self.endpoint);
         cookiedict = {"session": self.session_cookie}
-        self.session.session.cookies = cookiejar_from_dict(cookiedict)
+        cookies = requests.utils.cookiejar_from_dict(cookiedict)
+        self.session.cookies.update(cookies)
 
         r = self.session.open(mainpage)
-
-        try:
-            accountinfo = self.request('index')
-            self.authkey = accountinfo['authkey']
-            self.passkey = accountinfo['passkey']
-            self.userid = accountinfo['id']
-        except:
-            raise LoginException
+        self._get_account_info()
 
     def _login_username_password(self):
         '''Logs in user and gets authkey from server'''
+
+        if not self.username or self.username == "":
+            print("WARNING: username authentication attempted, but username not set, skipping.")
+            raise LoginException
+
         loginpage = '{0}/login.php'.format(self.endpoint)
         data = {'username': self.username,
                 'password': self.password}
@@ -116,13 +132,7 @@ class RED_OPS_API:
             r = self.session.post(loginpage, params=params, data=data)
             if r.status_code != 200:
                 raise LoginException
-        try:
-            accountinfo = self.request('index')
-            self.authkey = accountinfo['authkey']
-            self.passkey = accountinfo['passkey']
-            self.userid = accountinfo['id']
-        except:
-            raise LoginException
+        self._get_account_info()
 
     def logout(self):
         self.session.get('{0}/logout.php?auth={1}'.format(self.endpoint, self.authkey))
@@ -134,7 +144,7 @@ class RED_OPS_API:
 
         ajaxpage = '{0}/ajax.php'.format(self.endpoint)
         params = {'action': action}
-        if self.authkey:
+        if not self.api_key_authenticated and self.authkey:
             params['auth'] = self.authkey
         params.update(kwargs)
         r = self.session.get(ajaxpage, params=params, allow_redirects=False)
@@ -144,20 +154,9 @@ class RED_OPS_API:
             if parsed['status'] != 'success':
                 raise RequestException
             return parsed['response']
-        except ValueError:
-            raise RequestException
+        except ValueError as e:
+            raise RequestException(e)
 
-    def request_html(self, action, **kwargs):
-        while time.time() - self.last_request < self.rate_limit:
-            time.sleep(0.1)
-
-        ajaxpage = '{0}action'.format(self.endpoint)
-        if self.authkey:
-            kwargs['auth'] = self.authkey
-        r = self.session.get(ajaxpage, params=kwargs, allow_redirects=False)
-        self.last_request = time.time()
-        return r.content
-    
     def get_artist(self, id=None, format='MP3', best_seeded=True):
         res = self.request('artist', id=id)
         torrentgroups = res['torrentgroup']
@@ -180,36 +179,28 @@ class RED_OPS_API:
         res['torrentgroup'] = keep_releases
         return res
 
-    def get_candidates(self, skip=None, media=lossless_media):
-        if not media.issubset(lossless_media):
-            raise ValueError('Unsupported media type {0}'.format((media - lossless_media).pop()))
+    def snatched(self):
+        page = 0
+        while True:
+            response = self.request(
+                'user_torrents',
+                id=self.userid,
+                type='snatched',
+                limit=self.page_size,
+                offset=page * self.page_size
+            )
+            snatched = response['snatched']
+            if len(snatched) == 0:
+                break
+            print(f'Fetched snatched results {page * self.page_size} to '
+                  f'{(page + 1) * self.page_size - 1}')
+            for entry in snatched:
+                group_id = int(entry['groupId'])
+                torrent_id = int(entry['torrentId'])
+                yield group_id, torrent_id
+            page += 1
 
-        # gazelle doesn't currently support multiple values per query
-        # parameter, so we have to search a media type at a time;
-        # unless it's all types, in which case we simply don't specify
-        # a 'media' parameter (defaults to all types).
-
-        if media == lossless_media:
-            media_params = ['']
-        else:
-            media_params = ['&media={0}'.format(media_search_map[m]) for m in media]
-
-        list = ['uploaded', 'snatched']
-        for up_Down in list:
-            url = self.endpoint+'/torrents.php?type='+up_Down+'&userid='+str(self.userid)+'&format=FLAC' 
-            for mp in media_params:
-                page = 1
-                done = False
-                pattern = re.compile('torrents.php\?id=(\d+)&amp;torrentid=(\d+)')
-                while not done:
-                    content = self.session.get(url + mp + "&page=%s" % page).text
-                    for groupid, torrentid in pattern.findall(content):
-                        if skip is None or torrentid not in skip:
-                            yield int(groupid), int(torrentid)
-                    done = 'Next &gt;' not in content
-                    page += 1
-
-    def uploadOPS(self, group, torrent, new_torrent, format, description=[]):
+    def upload(self, group, torrent, new_torrent, format, description=[]):
         url = '{0}/upload.php?groupid={1}'.format(self.endpoint, group['group']['id'])
         self.session.open(url)
         form = self.session.select_form(selector='.create_form')
@@ -217,44 +208,6 @@ class RED_OPS_API:
         # requests encodes using rfc2231 in python 3 which php doesn't understand
         files = {'file_input': ('1.torrent', open(new_torrent, 'rb'), 'application/x-bittorrent')}
 
-        # MechanicalSoup 0.12.0+ now overwrites files with blank if a matching form field
-        # exists and is not disabled.
-        torrent_field = form.form.find('input', attrs={'id': 'file'})
-        if torrent_field:
-            torrent_field.attrs['disabled'] = 'disabled'
-
-        if torrent['remastered']:
-            form['remaster'] = True
-            form['remaster_year'] = str(torrent['remasterYear'])
-            form['remaster_title'] = torrent['remasterTitle']
-            form['remaster_record_label'] = torrent['remasterRecordLabel']
-            form['remaster_catalogue_number'] = torrent['remasterCatalogueNumber']
-        else:
-            form['remaster_year'] = ''
-            form['remaster_title'] = ''
-            form['remaster_record_label'] = ''
-            form['remaster_catalogue_number'] = ''
-
-        form['format'] = formats[format]['format']
-        form['bitrate'] = formats[format]['encoding']
-        form['media'] = torrent['media']
-
-        release_desc = '\n'.join(description)
-        if release_desc:
-            form['release_desc'] = release_desc
-
-        return self.session.submit_selected(files=files)
-
-    def uploadRED(self, group, torrent, new_torrent, format, description=[]):
-        url = '{0}/upload.php?groupid={1}'.format(self.endpoint, group['group']['id'])
-        self.session.open(url)
-        form = self.session.select_form(selector='.create_form')
-
-        # requests encodes using rfc2231 in python 3 which php doesn't understand
-        files = {'file_input': ('1.torrent', open(new_torrent, 'rb'), 'application/x-bittorrent')}
-
-        # MechanicalSoup 0.12.0+ now overwrites files with blank if a matching form field
-        # exists and is not disabled.
         torrent_field = form.form.find('input', attrs={'id': 'file'})
         if torrent_field:
             torrent_field.attrs['disabled'] = 'disabled'
